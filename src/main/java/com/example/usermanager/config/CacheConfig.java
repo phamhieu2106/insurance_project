d7bb089c07cc944435ee89e6d612eddd9251cec9
phamhieu2106 <phamvanhieu2106@gmail.com>
2024-06-14 09:24:32 +0700

    feat: redis cache for get customer

--- src/main/java/com/example/usermanager/config/CacheConfig.java
+++ src/main/java/com/example/usermanager/config/CacheConfig.java
@@ -1,15 +1,19 @@
 package com.example.usermanager.config;
 
+import com.example.usermanager.utils.convert.BytesToLocalDateConvert;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.convert.RedisCustomConversions;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 
 import java.time.Duration;
+import java.util.List;
 
 @Configuration
 @EnableCaching
@@ -32,12 +36,25 @@ public class CacheConfig {
     @Bean
     public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
         RedisCacheConfiguration cacheConfig = RedisCacheConfiguration.defaultCacheConfig()
-                .entryTtl(Duration.ofSeconds(300)) // Thiết lập TTL cho mục cache (ví dụ: 300 giây)
+                .entryTtl(Duration.ofSeconds(300)) // Thiết lập TTL cho mục cache ( 300 giây)
                 .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
 
         return RedisCacheManager.builder(connectionFactory)
                 .cacheDefaults(cacheConfig)
                 .build();
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        //
+        return template;
+    }
+
+    @Bean
+    public RedisCustomConversions redisCustomConversions(BytesToLocalDateConvert bytesToLocalDateTimeConverter) {
+        return new RedisCustomConversions(List.of(bytesToLocalDateTimeConverter));
     }
 
 }
--- src/main/java/com/example/usermanager/config/JacksonConfig.java
+++ src/main/java/com/example/usermanager/config/JacksonConfig.java
@@ -1,6 +1,7 @@
 package com.example.usermanager.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,8 +13,8 @@ public class JacksonConfig {
     public ObjectMapper objectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
         return objectMapper;
     }
-
 
 }
