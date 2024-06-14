c5b553d41185c61116ab0f10320e98076d29292d
phamhieu2106 <phamvanhieu2106@gmail.com>
2024-06-14 15:12:09 +0700

    feat: constaint date pattern

---
+++ src/main/java/com/example/usermanager/utils/contraint/DatePattern.java
@@ -0,0 +1,10 @@
+package com.example.usermanager.utils.contraint;
+
+public class DatePattern {
+
+    public static final String DATE_PATTERN = "yyyy-MM-dd'T'HH:mm:ss";
+
+    private DatePattern() {
+        super();
+    }
+}
